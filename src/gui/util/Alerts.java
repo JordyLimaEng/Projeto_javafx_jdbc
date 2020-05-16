a10f19e595ea591a6aff5452dc900a89d8adc1ae
JordyLimaEng <jordy.sousa@eng.ci.ufpb.br>
2020-05-16 02:45:32 -0300

    add  remove department

--- src/gui/util/Alerts.java
+++ src/gui/util/Alerts.java
@@ -1,7 +1,14 @@
 package gui.util;
 
+import java.util.Optional;
+
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TableCell;
+import model.entities.Department;
 
 public class Alerts {
 
@@ -11,5 +18,13 @@ public class Alerts {
 		alert.setHeaderText(header);
 		alert.setContentText(content);
 		alert.show();
+	}
+
+	public static Optional<ButtonType> showConfirmation(String title, String content) {
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle(title);
+		alert.setHeaderText(null);
+		alert.setContentText(content);
+		return alert.showAndWait();
 	}
 }
