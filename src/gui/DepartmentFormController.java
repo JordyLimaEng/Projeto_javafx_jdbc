1ad2dd41caaced2aa357f85ad2d4086d5a3aecf6
JordyLimaEng <jordy.sousa@eng.ci.ufpb.br>
2020-05-16 02:06:32 -0300

    passando obj department para department form view

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,17 +3,24 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
 	
 	private Department entity;
+	private DepartmentService service;
 	
 	public void setDepartment(Department entity) {
 		this.entity = entity;
@@ -34,14 +41,38 @@ public class DepartmentFormController implements Initializable {
 	@FXML
 	private Button btCancel;
 	
-	@FXML
-	public void onButtonSaveAction() {
-		System.out.println("ONBTSAVE");
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 	
 	@FXML
-	public void onButtonCancelAction() {
-		System.out.println("ONBTCancel");
+	public void onButtonSaveAction(ActionEvent event) {
+		if(entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if(service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			Utils.currentStage(event).close();
+		}catch(DbException e) {
+			Alerts.showAlert("erro ao salvar dado", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
+	private Department getFormData() {
+		Department obj = new Department();
+		
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+		return obj;
+	}
+
+	@FXML
+	public void onButtonCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
 	}
 	
 	
